f5f32ca3f0632c477d2c3dfb4976918369b6d026
rafaello129 <rr91175342@gmail.com>
2025-05-21 16:18:20 -0400

    a

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { InventarioPage } from './Inventario.tsx'
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <InventarioPage />
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
+  
   </StrictMode>,
 )
