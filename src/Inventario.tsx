d63a1cbe97d72fc8e94ba62a32023cdc9d383c3c
Gil240 <119063460+Gil240@users.noreply.github.com>
2025-05-21 18:15:13 -0500

    hola

---
+++ src/Inventario.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export let productos: any[] = [
+    {
+        id: 1,
+        nombre: "Jabón",
+        img: "src/assets/jabon.jpg"
+    },
+    {
+        id: 2,
+        nombre: "Jabón",
+        img: "src/assets/jabon.jpg"
+    }
+]
+
+export const InventarioPage = () => {
+
+    
+
+  return (
+    <div>
+        Hola
+    </div>
+  )
+}
+
